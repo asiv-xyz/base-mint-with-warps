020828fdba00d516b1c499bf43ef425dc9c75d4a
boosik-riverbank <boosik@riverbank.world>
2024-05-06 15:48:37 +0900

    test: set public url

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
-export const NEXT_PUBLIC_URL = 'https://mint.farcaster.xyz';
-export const ALLOWED_ORIGIN = 'mint.farcaster.xyz';
+export const NEXT_PUBLIC_URL = 'https://base-mint-with-warps-zeta.vercel.app';
+export const ALLOWED_ORIGIN = 'base-mint-with-warps-zeta.vercel.app';
 export const ZORA_COLLECTION_ADDRESS = '0xf5a3b6dee033ae5025e4332695931cadeb7f4d2b';
 export const ZORA_TOKEN_ID = '1';
 export const CARD_DIMENSIONS = {
