7cc52efb069e09c2fdc8f4ec2b3b212d7d50ad17
boosik-riverbank <boosik@riverbank.world>
2024-05-14 14:46:01 +0900

    fix: public url

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
-export const NEXT_PUBLIC_URL = 'https://base-mint-with-warps-zeta.vercel.app';
-export const ALLOWED_ORIGIN = 'base-mint-with-warps-zeta.vercel.app';
+export const NEXT_PUBLIC_URL = 'https://frames.surfy.network';
+export const ALLOWED_ORIGIN = 'frames.surfy.network';
 export const ZORA_COLLECTION_ADDRESS = '0x313714fc7bfffcbc5d1f60a6d7e3a3ccbef5cc36';
 export const ZORA_TOKEN_ID = '1';
 export const CARD_DIMENSIONS = {
